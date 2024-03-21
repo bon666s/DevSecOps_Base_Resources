59a41a6796b8611d1726e79f20c42cd2151d9b68
bon666s <161769095+bon666s@users.noreply.github.com>
2024-03-21 14:45:43 -0500

    Update CI_Quarkus-JenkinsFile.groovy

--- CI_Quarkus-JenkinsFile.groovy
+++ CI_Quarkus-JenkinsFile.groovy
@@ -62,7 +62,7 @@ pipeline{
       // Construye la imagen Docker utilizando el Dockerfile proporcionado
       //sh "docker build -t ${dockerImageName}:${dockerImageTag} -f src/main/docker/Dockerfile.jvm ."
       bat "docker ps"
-      bat "docker run --rm --network some-network -e DOCKER_TLS_CERTDIR=/certs -v some-docker-certs-client:/certs/client:ro -v C:/Jenkins/Agent/workspace/CI_Quarkus/:/datos docker:jijo sh"
+      bat "docker run --network some-network -e DOCKER_TLS_CERTDIR=/certs -v some-docker-certs-client:/certs/client:ro -v C:/Jenkins/Agent/workspace/CI_Quarkus/:/datos docker:jijo sh"
     }
   }
 }
