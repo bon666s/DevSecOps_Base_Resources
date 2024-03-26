b0e5cf9572315f0f53f9ed4656315e85c5faebfe
Alejandro Bermúdez <abermudez@gbm.net>
2024-03-26 11:26:41 -0500

    update docker clean

--- CIUtils/dockerClean.bat
+++ CIUtils/dockerClean.bat
@@ -18,7 +18,15 @@ echo Eliminando la imagen dockertrivy:latest...
 docker image rm dockertrivy:latest
 
 echo Eliminando la imagen docker:latest...
-docker image rm docker:latest
+
+SET IMAGE_NAME=docker:dind
+
+REM Paso 1: Eliminar contenedores (la línea específica de xargs se omite aquí)
+FOR /f "tokens=*" %%i IN ('docker ps -a --filter "ancestor=%IMAGE_NAME%" --format "{{.ID}}"') DO docker rm -f %%i
+
+REM Paso 2: Eliminar imagen
+docker rmi %IMAGE_NAME%
+
 
 echo Proceso completado.
 pause
